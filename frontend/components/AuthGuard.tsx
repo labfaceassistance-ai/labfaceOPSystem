e82a7abe2ba4e2c99acb0e917f6bdd601f5b53e3
Glason Nel Garganta <glasonnel.duenasgarganta@gmail.com>
2026-04-12 17:20:45 +0800

    fix: remove invalid ESLint comment from AuthGuard.tsx causing build error

--- frontend/components/AuthGuard.tsx
+++ frontend/components/AuthGuard.tsx
@@ -28,7 +28,6 @@ export default function AuthGuard({ children, allowedRoles }: AuthGuardProps) {
             }
         }
 
-        // eslint-disable-next-line react-hooks/set-state-in-effect
         setAuthorized(true);
     }, [router, allowedRoles]);
 
